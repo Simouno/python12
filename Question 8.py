b2314f6b68141ab7bb8e2de5ef765a3e371d2b5a
Simouno <58078127+Simouno@users.noreply.github.com>
2020-01-17 07:05:11 -0400

    Create Question 8.py

---
+++ Question 8.py
@@ -0,0 +1,8 @@
+from Tkinter import *
+import Tkinter
+Tkinter.title("Window")
+Tkinter.configure(width=500,height=200)
+Tkinter.configure(background="magenta")
+message=Label(fen, text="Salut le monde!")
+message=Label(fen, text="J'aime la programmaion!")
+Tkinter.Tk()
