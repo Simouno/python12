5619a5bdc4aaf6557588945f1cace334dc96a15d
Simouno <58078127+Simouno@users.noreply.github.com>
2020-01-17 07:23:09 -0400

    Create Question 10.py

---
+++ Question 10.py
@@ -0,0 +1,8 @@
+from Tkinter import *
+import Tkinter
+Tkinter.title("Message pour tout le monde")
+Tkinter.configure(width=50,height=500)
+Tkinter.configure(background="yellow")
+message=Label(fen, text="Text 1")
+message=Label(fen, text="Text 2")
+Tkinter.Tk()
