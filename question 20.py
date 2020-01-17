b09fedcad5bf545ed894a5780ec1d2f27e6bd660
Simouno <58078127+Simouno@users.noreply.github.com>
2020-01-17 15:55:05 -0800

    Update question 20.py

--- question 20.py
+++ question 20.py
@@ -1,1 +1,5 @@
-
+from Tkinter import *
+fen.Tk()
+fen.configure(width="600", height="600", bg="blue")
+toile=Canvas(fen, width="500", height="500", bg="blue")
+toile.create_arc(width="300", height="300" bg="green")
