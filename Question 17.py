66f18a5160e57ec6324a059b36d1d3fd9d9a0759
Simouno <58078127+Simouno@users.noreply.github.com>
2020-01-17 15:33:47 -0800

    Update Question 17.py

--- Question 17.py
+++ Question 17.py
@@ -1,1 +1,19 @@
-
+from Tkinter import *
+fen.Tk()
+fen.configure(width="500", hight="500")
+bou1=Button(fen, text="Boutton 1",bg="yellow")
+bou2=Button(fen, text="Boutton 1",bg="yellow")
+bou3=Button(fen, text="Boutton 1",bg="yellow")
+bou1.pack()
+bou2.pack()
+bou3.pack()
+def 1(event):
+	print("1")
+def 2(event):
+	print("2")
+def 3(event):
+	print("3")
+bou1=bind("<Button-1>",1)
+bou2=bind("<Button-1>",2)
+bou3=bind("<Button-1>",3)
+fen.mainloop()
