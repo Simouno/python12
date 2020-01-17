253dbd65d03e18f31f23c1d1ab849cd3a3b6d59c
Simouno <58078127+Simouno@users.noreply.github.com>
2020-01-17 12:32:43 -0400

    Create Question 11.py

---
+++ Question 11.py
@@ -0,0 +1,12 @@
+from Tkinter import *
+import Tkinter
+Tkinter.title("Message pour tout le monde")
+Tkinter.configure(width=500,height=500)
+Tkinter.configure(background="yellow")
+message=Label(fen, text="Text 1")
+message=Label(fen, text="Text 2")
+bou=Button(fen,text="Bouton 1", bg="black", fg="yellow")
+bou.pack()
+bou=Bouton(fen,text="Bouton 2", bg="black", fg="yellow")
+bou.pack()
+Tkinter.Tk()
