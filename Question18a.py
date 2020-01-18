c66c2ce381a1ad8af893a881e6421682f9152a15
Simouno <58078127+Simouno@users.noreply.github.com>
2020-01-17 16:07:25 -0800

    Delete Question18a.py

--- Question18a.py
+++
@@ -1,6 +0,0 @@
-from Tkinter import *
-fen.Tk()
-fen.configure(width="400", height="700", bg="cyan")
-toile=Canvas(fen, width="300", height="400", bg="blue")
-toile.pack()
-toile.create_rectangle(width="200", height="500")
