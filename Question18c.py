28c3c8ea95d71d0713af337abcfaafaafc75e4df
Simouno <58078127+Simouno@users.noreply.github.com>
2020-01-17 16:07:11 -0800

    Update Question18c.py

--- Question18c.py
+++ Question18c.py
@@ -1,7 +1,7 @@
 from Tkinter import *
 fen.Tk()
 fen.configure(width="400", height="700", bg="cyan")
-toile=Canvas(fen, width="300", height="400", bg="blue")
+toile=Canvas(fen, width="300", height="400", bg="red")
 toile.pack()
-toile.create_rectangle(width="200", height="500")
-toile.create_arc(width="100", height="400")
+rectangle = toile.create_rectangle(200, 200, 500, 500, fill="yellow")
+arc = toile.create_arc(100, 100, 500, 500, fill="green")
