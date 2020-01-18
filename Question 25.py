cf38f3c5d08b87c9ca5ffc46c3694976746353db
Simouno <58078127+Simouno@users.noreply.github.com>
2020-01-17 18:07:15 -0800

    Update Question 25.py

--- Question 25.py
+++ Question 25.py
@@ -3,7 +3,7 @@ import time
 fen.Tk()
 fen.configure(width="600", height="600", bg="blue")
 toile=Canvas(fen, width="500", height="500", bg="blue")
-rec = toile.create_rectangle(100,100,30,30, fill="green")
+rec = toile.create_oval(0,0,30,30, fill="green")
 while 1:
 	toile.move(rec, 100, 0)
 	time.sleep(1)
