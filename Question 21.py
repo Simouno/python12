d92cb2710cb9466d06308ff89d672ab5661978c2
Simouno <58078127+Simouno@users.noreply.github.com>
2020-01-17 16:44:35 -0800

    Update Question 21.py

--- Question 21.py
+++ Question 21.py
@@ -1,7 +1,11 @@
 from Tkinter import *
-import time *
+import time
 fen.Tk()
-fen.configure(width="800", height="800", bg="black")
-toile=Canvas(fen, width="500", height="500", bg="red")
-toile.create_rectangle(width="100",height="30")
-toile.move(1)
+fen.configure(width="600", height="600", bg="blue")
+toile=Canvas(fen, width="500", height="500", bg="blue")
+rec = toile.create_rectangle(100,100,30,30, fill="green")
+while 1:
+	toile.move(rec, 100, 0)
+	time.sleep(1)
+	toile.update()
+fen.mainloop()
