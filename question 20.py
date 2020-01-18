c7b1a3a2df064e6dedad15f85745becba02a02f9
Simouno <58078127+Simouno@users.noreply.github.com>
2020-01-17 16:44:23 -0800

    Update question 20.py

--- question 20.py
+++ question 20.py
@@ -1,6 +1,7 @@
 from Tkinter import *
+import time
 fen.Tk()
 fen.configure(width="600", height="600", bg="blue")
 toile=Canvas(fen, width="500", height="500", bg="blue")
-toile.create_arc(width="300", height="300" bg="green")
-fen.mainloop()
+rec = toile.create_rectangle(100,100,30,30, fill="green")
+
