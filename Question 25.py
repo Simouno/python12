6bc48323c2c40b267abd7452a8d3f3d6db86b1c7
Simouno <58078127+Simouno@users.noreply.github.com>
2020-01-18 05:04:05 -0800

    Update Question 25.py

--- Question 25.py
+++ Question 25.py
@@ -3,9 +3,16 @@ import time
 fen.Tk()
 fen.configure(width="600", height="600", bg="blue")
 toile=Canvas(fen, width="500", height="500", bg="blue")
-rec = toile.create_oval(0,0,30,30, fill="green")
+balle = toile.create_oval(0,0,30,30, fill="green")
+x=50
+y=50
 while 1:
-	toile.move(rec, 100, 0)
-	time.sleep(1)
+	toile.move(rec, x, y)
+	pos=toile.coords(balle)
+	if pos[2]>=600
+		x=-x
+	if pos[3]>=600
+		y=-y
+	time.sleep(0.1)
 	toile.update()
 fen.mainloop()
