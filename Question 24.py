e1d5f3a1eb76551a3175f739337414a1462d0514
Simouno <58078127+Simouno@users.noreply.github.com>
2020-01-18 04:59:14 -0800

    Update Question 24.py

--- Question 24.py
+++ Question 24.py
@@ -3,9 +3,14 @@ import time
 fen.Tk()
 fen.configure(width="600", height="600", bg="blue")
 toile=Canvas(fen, width="500", height="500", bg="blue")
-rec = toile.create_rectangle(100,100,30,30, fill="green")
+rec = toile.create_rectangle(0,100,0,30, fill="green")
 while 1:
 	toile.move(rec, 100, 0)
-	time.sleep(1)
+	pos=toile.coords(rec))
+	if pos[1]>=0
+		toile.move(rec, 100, 0)
+	if pos[1]<=0
+		toile.move(rec, -100, 0)
+	time.sleep(0.25)
 	toile.update()
 fen.mainloop()
